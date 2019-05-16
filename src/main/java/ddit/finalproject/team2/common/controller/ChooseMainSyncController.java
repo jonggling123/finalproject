7eece58166cb75a6b820338376cfc3d1d9e29987
jek <jek@DESKTOP-GL9MQ15>
2019-05-16 13:54:50 +0900

    KJE

--- src/main/java/ddit/finalproject/team2/common/controller/ChooseMainSyncController.java
+++ src/main/java/ddit/finalproject/team2/common/controller/ChooseMainSyncController.java
@@ -5,25 +5,20 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.jmx.export.annotation.ManagedAttribute;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import ddit.finalproject.team2.common.service.IUserFindService;
-
 import ddit.finalproject.team2.common.service.KJE_IFaceIdService;
-import ddit.finalproject.team2.util.AuthConstants;
 import ddit.finalproject.team2.util.AuthorityUtil;
+import ddit.finalproject.team2.util.constant.AuthConstants;
 import ddit.finalproject.team2.vo.UserVo;
 
 /**
